201cb9d93484e554289b3f5bf1f6b2c6813be6ca
DilshanGAM <malithdilshan27@gmail.com>
2026-04-02 21:05:59 +0530

    day 21 ✏️

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
       <div className='w-full h-screen flex justify-center items-center bg-primary text-secondary'>
         <Toaster position='top-right'/>
         <Routes>
-          <Route path='/' element={<HomePage />} />
+          <Route path='/*' element={<HomePage />} />
           <Route path='/admin/*' element={<AdminPage />} />
           <Route path='/test' element={<TestPage />} />
           <Route path="/login" element={<LoginPage />} />
